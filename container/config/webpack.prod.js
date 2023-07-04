389bb48b7a39c5e3953351493e6a083c73fe969f
Mahadik <dmahadik@luxoft.com>
2023-07-04 15:27:35 +0200

    commits partial learning to test container deploy script

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,19 +1,20 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contentHash].js'
+        filename: '[name].[contenthash].js'
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'marketing',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './MarketingApp': './src/bootstrap'
+            name: 'container',
+            remotes: {
+                marketing: `marketing@${domain}/marketing/remoteEntry.js`
             },
             shared: packageJson.dependencies
         })
